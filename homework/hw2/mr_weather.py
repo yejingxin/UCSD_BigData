68a61fb5fe88a1754c47af3b5ae045884f41865a
Jingxin <yejingxin@live.cn>
2014-05-24 04:53:11 +0000

    some files deleted

--- homework/hw2/mr_weather.py
+++ homework/hw2/mr_weather.py
@@ -26,14 +26,14 @@ class MRWeather(MRJob):
                 key = (elements[0],elements[2])
                 #out=(elements[0],elements[2]), 
             else:
-                key = 'Useless'
+                key = ('Useless','Useless')
                 meas = 0
                 Ndays = 0
         except Exception, e:
             stderr.write('Error in line:\n'+line)
             stderr.write(e)
             self.increment_counter('MrJob Counters','mapper-error',1)
-            key = 'error'
+            key = ('error','error')
             meas = 0
             Ndays = 0
 
@@ -49,20 +49,6 @@ class MRWeather(MRJob):
             sum1 = sum1 + meas
             sum2 = sum2 + Ndays
         yield word, (sum1, sum2)
-#    def combiner(self, word, counts):
-#        self.increment_counter('MrJob Counters','combiner',1)
-#        l_counts=[c for c in counts]
-#        sum1 = 0
-#        sum2 = 0
-#        for e in l_counts:
-#            sum1 = sum1 + float(e[0])
-#            sum2 = sum2 + float(e[1])
-#        sum3 = (sum1,sum2)
-#        yield (word, sum3)
-        #l_counts=[c for c in counts]  # extract list from iterator
-        #S=sum(l_counts)
-        #logfile.write('combiner '+word+' ['+','.join([str(c) for c in l_counts])+']='+str(S)+'\n')
-        #yield (word, S)
 
     def reducer(self, word, counts):
         self.increment_counter('MrJob Counters','reducer',1)
@@ -71,11 +57,10 @@ class MRWeather(MRJob):
         for meas,Ndays in counts:
             sum1 = sum1 + meas
             sum2 = sum2 + Ndays
-        yield word, (sum1, sum2)
-        #l_counts=[c for c in counts]  # extract list from iterator
-        #S=sum(l_counts)
-        #logfile.write('reducer '+word+' ['+','.join([str(c) for c in l_counts])+']='+str(S)+'\n')
-        #yield (word, S)
+        if sum1 == 2 and sum2>200:
+            yield '%s %s' %(word[0],word[1]), sum2
+        else:
+            yield 'Useless Useless',0
 
 if __name__ == '__main__':
     MRWeather.run()
